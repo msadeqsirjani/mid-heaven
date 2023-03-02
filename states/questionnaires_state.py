c95908239cc132d711f10ebd4c3201aea38f98d2
Mohammad Sadeq Sirjani <45625225+msadeqsirjani@users.noreply.github.com>
2023-03-02 18:56:15 +0330

    feat: save questionaries state

---
+++ states/questionnaires_state.py
@@ -0,0 +1,14 @@
+from aiogram.dispatcher.filters.state import StatesGroup, State
+
+
+class QuestionnairesState(StatesGroup):
+    rendering_mode = State()
+    style = State()
+    number_of_storey = State()
+    roof_type = State()
+    number_of_material = State()
+    garage = State()
+    roof_type_advanced = State()
+    details = State()
+    overhang = State()
+    style_advanced = State()
