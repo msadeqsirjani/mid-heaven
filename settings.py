e46eeec4f51a1c52c48b57972d1c7f39014d341c
Mohammad Sadeq Sirjani <45625225+msadeqsirjani@users.noreply.github.com>
2023-03-03 13:20:19 +0330

    feat: setup open api token

--- settings.py
+++ settings.py
@@ -6,4 +6,5 @@ env.read_env()
 
 REPLICATE_API_TOKEN = env.str('REPLICATE_API_TOKEN')
 TELEGRAM_TOKEN = env.str('TELEGRAM_TOKEN')
+OPEN_AI_API_TOKEN = env.str('OPEN_AI_API_TOKEN')
 BASE_DIR = Path(__file__).resolve().parent
