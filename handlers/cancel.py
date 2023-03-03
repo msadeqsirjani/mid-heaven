78bcf276a8fddbb295631344cc1349dd5f3bd115
Mohammad Sadeq Sirjani <45625225+msadeqsirjani@users.noreply.github.com>
2023-03-03 13:19:11 +0330

    feat: update commands more general

--- handlers/cancel.py
+++ handlers/cancel.py
@@ -1,11 +1,12 @@
 from aiogram import types
 from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters import Text
 from keyboards.replay_markup_button import make_button
 from loader import dispatcher
 from constant import create, cancel
 
 
-@dispatcher.message_handler(commands=['cancel'], state="*")
+@dispatcher.message_handler(Text(contains='cancel'), state="*")
 async def cancel_bot(message: types.Message, state: FSMContext):
     await state.finish()
     await message.answer(text="Good. We will go back.", reply_markup=make_button(words=[create], row_width=2))
